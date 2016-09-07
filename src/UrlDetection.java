8c91aa2c5f27f9fc6e7756ff222d27c5bb2d0c17
Mateus <a.mateuslol@gmail.com>
2016-09-07 15:38:34 -0300

    teste

---
+++ src/UrlDetection.java
@@ -0,0 +1,66 @@
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UrlDetection {
+	
+	public List<String> containedUrls = new ArrayList<String>();
+	public int CountUrl;
+	
+	
+	//======================= Sets =========================// 
+	
+	
+	
+	
+	
+	//======================================================// 
+
+	
+	//======================= Gets =========================// 
+	
+	public int getCountUrl(){	
+		return this.CountUrl;
+	}
+	
+	public List<String> getContainedUrls(){
+		return this.containedUrls;
+	}
+	
+	//======================================================// 
+	
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println(extractUrls("Welcome to https://stackoverflow.com/ and here is another link http://www.google.com/ \n which is a great search engine"));
+		System.out.println();
+
+	}
+		
+	
+	/**
+	 * Returns a list with all links contained in the input
+	 */
+	public static List<String> extractUrls(String text)
+	{
+	    List<String> containedUrls = new ArrayList<String>();
+	    String urlRegex = "((https?|ftp|gopher|telnet|file):((//)|(\\\\))+[\\w\\d:#@%/;$()~_?\\+-=\\\\\\.&]*)";
+	    Pattern pattern = Pattern.compile(urlRegex, Pattern.CASE_INSENSITIVE);
+	    Matcher urlMatcher = pattern.matcher(text);
+
+	    while (urlMatcher.find())
+	    {
+	        containedUrls.add(text.substring(urlMatcher.start(0),
+	                urlMatcher.end(0)));
+	    }
+
+	    return containedUrls;
+	}
+	
+	
+
+}
---
+++ src/Weblinking.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Weblinking {
+
+	public static void main(String[] args) {
+		
+		Scanner teclado = new Scanner(System.in);
+		
+		String texto = teclado.nextLine();
+		
+
+	}
+	
+	String frase = new String();
+	
+	
+	public int ContadorSentença(){
+		
+		return 0;
+	}
+	
+	
+	public float WeblinkingCalc(){
+		
+		
+		
+		return 0;
+		
+	}
+	
+
+}
